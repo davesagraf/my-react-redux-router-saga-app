ac504fb6a59fbf86ae8fd9562114dfb1a6e0d5d7
dave <d.saguev@leviossa.net>
2022-05-20 17:32:34 +0300

    changed comments

--- src/components/ShowComments.jsx
+++ src/components/ShowComments.jsx
@@ -10,6 +10,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCurrentPost } from "../actions/postAction";
 import { grey } from "@mui/material/colors";
 
+import { Card, CardContent, CardActions, Button } from "@mui/material";
+import moment from "moment";
 
 export function ShowComments(props) {
   const commentEl = useRef(null);
@@ -27,16 +29,23 @@ export function ShowComments(props) {
     setOpen(!open);
     commentEl.current.focus();
     const showCommentsButtonId = event.target.id;
-    dispatch(getCurrentPost(showCommentsButtonId))
+    dispatch(getCurrentPost(showCommentsButtonId));
   };
 
-  let newComments = comments.filter(comment => comment.post_id === thisCommentPostId);
+  let newComments = comments.filter(
+    (comment) => comment.post_id === thisCommentPostId
+  );
 
   return (
     <>
       <List
         id={thisCommentPostId}
-        sx={{ width: "100%", maxWidth: 360, backgroundColor: "transparent", transform: "translate(0em, -5em)" }}
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          backgroundColor: "transparent",
+          transform: "translate(0em, -5em)",
+        }}
       >
         <ListItemButton
           ref={commentEl}
@@ -58,10 +67,10 @@ export function ShowComments(props) {
           <Grid
             item
             sx={{
-            width: "50em",
-            display: "flex",
-            flexDirection: "column",
-            cursor: "pointer",
+              width: "50em",
+              display: "flex",
+              flexDirection: "column",
+              cursor: "pointer",
             }}
           ></Grid>
           {open ? (
@@ -73,60 +82,72 @@ export function ShowComments(props) {
 
         <Collapse id={thisCommentPostId} in={open} timeout="auto" unmountOnExit>
           <List id={thisCommentPostId} component="div" disablePadding>
-            <ListItemButton id={thisCommentPostId}       sx={{
-            width: "50em",
-            display: "flex",
-            flexDirection: "column",
-            cursor: "pointer",
-            marginTop: "1em",
-            marginBottom: "1em"
-            }}>
-              { newComments.map((thisPostComment) => (
-                  <Grid
-                    item
-                    sx={{
-                      width: "50em",
-                      display: "flex",
-                      flexDirection: "column",
-                      cursor: "pointer",
-                      marginTop: "3em",
-                      marginBottom: "3em"
-                    }}
-                  >
-                        <>
-                          <Typography
-                            sx={{
-                              fontSize: 20,
-                              backgroundColor: grey[400],
-                              transform: "translate(0em, -4em)",
-                            }}
-                            color="white"
-                            gutterBottom
-                          >Comment # {" " + thisPostComment.id}</Typography>
-                          <Typography
-                            sx={{ fontSize: 18, transform: "translate(0em, -3em)" }}
-                            color="text.secondary"
-                            gutterBottom
-                          >
-                            {thisPostComment.title}
-                          </Typography>
-                          <Typography
-                            sx={{ fontSize: 13, transform: "translate(0em, -2.7em)" }}
-                            color="text.secondary"
-                            gutterBottom
-                          >
-                           Comment AuthorID: {" " + " " + " " + thisPostComment.user_id}
-                          </Typography>
-                          <Typography
-                            sx={{ fontSize: 13, transform: "translate(0em, -2em)" }}
-                            color="text.secondary"
-                            gutterBottom
-                          >
-                           Created at: {" " + " " + " " + thisPostComment.createdAt}
-                          </Typography>
-                        </>
-                  </Grid>
-                ))}
+            <ListItemButton
+              id={thisCommentPostId}
+              sx={{
+                width: "50em",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                cursor: "pointer",
+                marginTop: "1em",
+                marginBottom: "1em",
+              }}
+            >
+              {newComments.map((thisPostComment) => (
+                <Card
+                  elevation={5}
+                  id={thisPostComment.id}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-end",
+                    background: grey[50],
+                    height: 250,
+                    width: 500,
+                    lineHeight: "60px",
+                    marginBottom: "5em",
+                    marginTop: "5em",
+                    borderRadius: "0.5em",
+                  }}
+                >
+                  <CardContent sx={{ marginBottom: "auto" }}>
+                    <Typography
+                      sx={{ mb: 1.5, fontSize: 14 }}
+                      color="text.secondary"
+                    >
+                      {"Comment #: " + thisPostComment.id}
+                    </Typography>
+
+                    <Typography
+                      sx={{ fontSize: 24 }}
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      {"Title: " + thisPostComment.title}
+                    </Typography>
+
+                    <Typography
+                      sx={{ mb: 1.5, fontSize: 14 }}
+                      color="text.secondary"
+                    >
+                      {"Author: " + thisPostComment.user_id}
+                    </Typography>
+                    <Typography
+                      sx={{ mb: 1.5, fontSize: 14 }}
+                      color="text.secondary"
+                    >
+                      {"Created At: " +
+                        moment(thisPostComment.createdAt).format(
+                          "MMMM Do YYYY, h:mm:ss a"
+                        )}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Expand Post</Button>
+                  </CardActions>
+                </Card>
+              ))}
               <ListItemText primary="Comments" />
             </ListItemButton>
           </List>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewPost, getAllPosts } from "../actions/postAction";
 import { Box, Button, Typography } from "@mui/material";
-import { Paper } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { NewPostInput } from "../components/NewPostInput";
@@ -10,11 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { ShowComments } from "../components/ShowComments";
 import moment from "moment";
 
-import {
-  Card,
-  CardContent,
-  CardActions,
-} from "@mui/material";
+import { Card, CardContent, CardActions } from "@mui/material";
 
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
@@ -49,7 +44,9 @@ export default function MainPage() {
     dispatch(getAllPosts());
   }, [dispatch]);
 
-  const theme = createTheme({ palette: { mode: "light", background: lightBlue[300]} });
+  const theme = createTheme({
+    palette: { mode: "light", background: lightBlue[300] },
+  });
 
   const navigate = useNavigate();
 
@@ -146,99 +143,60 @@ export default function MainPage() {
               {posts.map((post, index) => (
                 <>
                   <Tooltip key={index} title="click to see the post">
-                  <Card
-                  elevation={5}
-                  id={post.id}
-                  onClick={() => {
-                    navigate(`/post/${post.id}`);
-                  }}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-end",
-          background: lightBlue[50],
-          height: 250,
-          width: 500,
-          lineHeight: "60px",
-          marginBottom: "5em",
-          marginTop: "5em",
-          marginLeft: "30vw",
-          borderRadius: "0.5em",
-        }}
-      >
-        <CardContent sx={{ marginBottom: "auto" }}>
-          <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-            {"Title: " + post.title}
-          </Typography>
-          <Typography sx={{ mb: 1.5, fontSize: 14 }} color="text.secondary">
-            {"Description: " + post.description}
-          </Typography>
-          <Typography sx={{ mb: 1.5, fontSize: 14 }} color="text.secondary">
-            {"Author: " + post.user_id}
-          </Typography>
-          <Typography sx={{ mb: 1.5, fontSize: 14 }} color="text.secondary">
-            {"Created At: " + moment(post.createdAt).format('MMMM Do YYYY, h:mm:ss a') }
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Expand Post</Button>
-        </CardActions>
-      </Card>
-                    
-                    {/* <Item
+                    <Card
                       elevation={5}
                       id={post.id}
                       onClick={() => {
                         navigate(`/post/${post.id}`);
                       }}
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-end",
+                        background: lightBlue[50],
+                        height: 250,
+                        width: 500,
+                        lineHeight: "60px",
+                        marginBottom: "5em",
+                        marginTop: "5em",
+                        marginLeft: "30vw",
+                        borderRadius: "0.5em",
+                      }}
                     >
-                      <Typography
-                        sx={{
-                          fontSize: 20,
-                          backgroundColor: blue[600],
-                          transform: "translate(0em, -1.5em)",
-                        }}
-                        color="white"
-                        gutterBottom
-                      >
-                        Post Title:
-                        {" " + " " + " " + post.title}
-                      </Typography>
-                      <Typography
-                        sx={{ mb: 1.5, fontSize: 16, boxSizing: "border-box" }}
-                        color="text.secondary"
-                      >
-                        Post Description:
-                        {" " + " " + " " + post.description}
-                      </Typography>
-                      <Typography
-                        sx={{
-                          mb: 1.5,
-                          fontSize: 16,
-                          boxSizing: "border-box",
-                          transform: "translate(0em, 5em)",
-                        }}
-                        color="text.secondary"
-                      >
-                        Post AuthorID:
-                        {" " + " " + " " + post.user_id}
-                      </Typography>
-                      <Typography
-                        sx={{
-                          mb: 1.5,
-                          fontSize: 13,
-                          boxSizing: "border-box",
-                          transform: "translate(0em, 6em)",
-                        }}
-                        color="text.secondary"
-                      >
-                        Created at:
-                        {" " + " " + " " + post.createdAt}
-                      </Typography>
-                    </Item> */}
-
-
-
+                      <CardContent sx={{ marginBottom: "auto" }}>
+                        <Typography
+                          sx={{ fontSize: 24 }}
+                          color="text.secondary"
+                          gutterBottom
+                        >
+                          {"Title: " + post.title}
+                        </Typography>
+                        <Typography
+                          sx={{ mb: 1.5, fontSize: 14 }}
+                          color="text.secondary"
+                        >
+                          {"Description: " + post.description}
+                        </Typography>
+                        <Typography
+                          sx={{ mb: 1.5, fontSize: 14 }}
+                          color="text.secondary"
+                        >
+                          {"Author: " + post.user_id}
+                        </Typography>
+                        <Typography
+                          sx={{ mb: 1.5, fontSize: 14 }}
+                          color="text.secondary"
+                        >
+                          {"Created At: " +
+                            moment(post.createdAt).format(
+                              "MMMM Do YYYY, h:mm:ss a"
+                            )}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small">Expand Post</Button>
+                      </CardActions>
+                    </Card>
                   </Tooltip>
                   <Grid
                     item
